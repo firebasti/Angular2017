c6b9f99055327f0af20a7324f3dadc62ae58ce9d
Sebastian Walter <sebastian.walter1@allianz.de>
2017-05-16 13:20:04 +0200

    Tada

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,23 +5,14 @@ import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { InfoBoxComponent } from './info-box/info-box.component';
-<<<<<<< HEAD
 import { BookListComponent } from './book-list/book-list.component';
-=======
-import { MouseCursorComponent } from './mouse-cursor/mouse-cursor.component';
-import { TitleBoxComponent } from './title-box/title-box.component';
->>>>>>> 4b4b8b0f857eb387b6fd106a563ad0a930128f09
+
 
 @NgModule({
   declarations: [
     AppComponent,
     InfoBoxComponent,
-<<<<<<< HEAD
     BookListComponent,
-=======
-    MouseCursorComponent,
-    TitleBoxComponent
->>>>>>> 4b4b8b0f857eb387b6fd106a563ad0a930128f09
   ],
   imports: [
     BrowserModule,
