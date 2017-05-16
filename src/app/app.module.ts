6107e81f47a38d0887d60168494ad55ab23aa532
Sebastian Walter <sebastian.walter1@allianz.de>
2017-05-16 16:33:55 +0200

    asdf

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,13 +7,15 @@ import { AppComponent } from './app.component';
 import { InfoBoxComponent } from './info-box/info-box.component';
 import { BookListComponent } from './book-list/book-list.component';
 import {BookDataService} from "./shared/book-data.service";
-import {routing} from './app.routing'
+import {routing} from './app.routing';
+import { BookDetailComponent } from './book-detail/book-detail.component'
 
 @NgModule({
   declarations: [
     AppComponent,
     InfoBoxComponent,
     BookListComponent,
+    BookDetailComponent,
   ],
   imports: [
     BrowserModule,
--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,11 +1,15 @@
 import { Routes, RouterModule } from '@angular/router';
 import {ModuleWithProviders} from '@angular/core'
 import {BookListComponent} from "./book-list/book-list.component";
+import {InfoBoxComponent} from "./info-box/info-box.component";
+import {BookDetailComponent} from "./book-detail/book-detail.component";
 
 
-const routes: Routes = [{
-  path: 'books',
-  component: BookListComponent
-}];
+const routes: Routes = [
+  {path: '', redirectTo: '/books', pathMatch: 'full'},
+  {path: 'books', component: BookListComponent},
+  {path: 'books/:isbn', component: BookDetailComponent},
+  {path: '**',component: InfoBoxComponent}
+];
 
 export const routing: ModuleWithProviders = RouterModule.forRoot(routes);
